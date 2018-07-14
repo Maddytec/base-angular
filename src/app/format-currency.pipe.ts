990cc27ad8da6837b6ab7a94e202bb55879276f0
Madson Silva <maddytec@gmail.com>
2018-07-14 20:31:48 -0300

    Commit initical

--- src/app/format-currency.pipe.ts
+++ src/app/format-currency.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatCurrencyPipe implements PipeTransform {
 
-  transform(value: any, locale = 'Pt-Br': any): any {
+  transform(value: any, locale = 'Pt-Br'): any {
     return Intl.NumberFormat(locale, {style: 'currency', currency: 'BRL'} ).format(value);
   }
 
